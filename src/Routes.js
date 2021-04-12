55470110096b8b5c6ae6015dc1fd42e02f0aad15
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-04-12 16:47:24 -0700

    Added base routes and core/user components

---
+++ src/Routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { 
+  BrowserRouter, 
+  Switch, 
+  Route 
+} from 'react-router-dom';
+import Signup from './user/Signup';
+import Signin from './user/Signin';
+import Home from './core/Home';
+
+const Routes = () => {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/signin" component={Signin} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
