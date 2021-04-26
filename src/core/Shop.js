5d9d9193ae643573052ca5fad20b0e925c4e6191
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-04-26 14:32:09 -0700

    Added fetched filtered product data to cards

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -98,9 +98,7 @@ const Shop = () => {
           <h2 className="mb-4">Products</h2>
           <div className="row">
             {filteredResults.map((product, i) => (
-              <div key={i} className="col-6 mb-3">
-                <Card product={product} />
-              </div>
+              <Card key={i} product={product} />
             ))}
           </div>
         </div>
