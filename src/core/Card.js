f78895e2e9e70f77cd879647dd331f9d6be29103
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-04-29 11:25:36 -0700

    Changed minor <p> text

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import moment from "moment";
 import ShowImage from "./ShowImage";
 
 const Button = styled.button`
@@ -23,8 +24,12 @@ const Card = ({ product, showViewProductButton = true }) => {
       <div className="card-header">{product.name}</div>
       <div className="card-body">
         <ShowImage item={product} url="product" />
-        <p>{product.description.substring(0, 100)}</p>
-        <p>${product.price}</p>
+        <p className="lead mt-2">{product.description.substring(0, 100)}</p>
+        <p className="black-9">${product.price}</p>
+        <p className="black-8">
+          Category: {product.category && product.category.name}
+        </p>
+        <p className="black-8">Added {moment(product.createdAt).fromNow()}</p>
         {showViewButton(showViewProductButton)}
         <Button className="btn btn-outline-warning mt-2 mb-2">
           Add to Cart
