121824c12d9b5599752f47bba6d1efce73f7bbce
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-05-20 09:26:23 -0700

    Added methods for handling errors and generating data to be sent from front end payment drop-in to the back end

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -53,26 +53,63 @@ const Checkout = ({ products }) => {
     );
   };
 
-  //
+  // method for generating data to be sent to the back end
+  const buy = () => {
+    // send the notice to your server
+    // nonce = data.instance.rrquestPaymentMethod
+    let nonce;
+    let getNonce = data.instance
+      .requestPaymentMethod()
+      .then((data) => {
+        console.log(data);
+        nonce = data.nonce;
+        // once you have nonce (card type, card number) send nonce as
+        // 'paymentMethodNonce' and also total to be charged
+        console.log(
+          "send nonce and total to process: ",
+          nonce,
+          getTotal(products)
+        );
+      })
+      .catch((error) => {
+        console.log("dropin error: ", error);
+        setData({ ...data, error: error.message });
+      });
+  };
+
+  // shows payment drop-in and sets instance state data to that of the the drop-in
+  // instance
   const showDropIn = () => (
-    <div>
+    <div onBlur={() => setData({ ...data, error: "" })}>
       {data.clientToken !== null && products.length > 0 ? (
         <div>
           <DropIn
             options={{
               authorization: data.clientToken,
             }}
-            onInstance={(instance) => (instance = instance)}
+            onInstance={(instance) => (data.instance = instance)}
           />
-          <button className="btn btn-success">Checkout</button>
+          <button onClick={buy} className="btn btn-success">
+            Pay
+          </button>
         </div>
       ) : null}
+    </div>
+  );
+
+  const showError = (error) => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
     </div>
   );
 
   return (
     <div>
       <h2>Total: ${getTotal()}</h2>
+      {showError(data.error)}
       {showCheckout()}
     </div>
   );
